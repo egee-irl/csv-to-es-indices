8fcfcfb887dedba5ed7d9cfd997d91ad6ad9d3f1
egee-irl <brian@egee.io>
2018-08-08 18:26:42 -0700

    create index if not exist

--- src/main.ts
+++ src/main.ts
@@ -1,76 +1,40 @@
-let elasticsearch = require("elasticsearch");
-let fs = require("fs");
-let csv = require("fast-csv");
+import * as elasticsearch from "elasticsearch";
+import * as fs from "fs";
+import csv from "fast-csv";
 
-let stream = fs.createReadStream("1987.csv");
-let client = new elasticsearch.Client({
+const stream = fs.createReadStream("1987.csv");
+const client = new elasticsearch.Client({
   host: "https://search-eg-cluster-44ixoq4bigmmkxfr6akgx2qe3m.us-west-2.es.amazonaws.com/",
   log: "trace"
 });
 
-let csvStream = csv({
-  headers: true,
-  ignoreEmpty: true
-})
-    .on("data", function(data: any) {
-      client.index({
-        index: "helloworld",
-        type: "thing",
-        body: {
-          data
-        }
-      }).catch((r: any) => {
-        console.log(r);
-      });
-    })
-    .on("end", function() {
-         console.log("done");
+const exists = client.exists({
+  index: "helloworld",
+  type: "thing",
+  id: "1"
+}).then((r: any) => {
+  if (r == false) {
+    client.indices.create({
+      "index": "helloworld"
     });
+  }
+});
 
-stream.pipe(csvStream);
-
-// data = {
-//   "LOL!": "uhh",
-//   "another": 123
-// }
-
-// client.index({
-//   index: "helloworld",
-//   type: "thing",
-//   body: {
-//     data
-//   }
-// }).catch((r) => {
-//   console.log(r)
-// })
-
-// client.indices.create({
-//   "index": "helloworld"
-// })
-
-// client.ping({
-//     // ping usually has a 3000ms timeout
-//     requestTimeout: 1000
-//   }, function (error) {
-//     if (error) {
-//       console.trace('elasticsearch cluster is down!');
-//     } else {
-//       console.log('All is well');
+// const csvStream = csv({
+//   headers: true,
+//   ignoreEmpty: true
+// }).on("data", function(data: any) {
+//   client.index({
+//     index: "helloworld",
+//     type: "thing",
+//     body: {
+//       data
 //     }
+//   }).catch((r: any) => {
+//     console.log(r);
 //   });
-
-// const response = client.search({
-//   index: 'twitter',
-//   type: 'tweets',
-//   body: {
-//     query: {
-//       match: {
-//         body: 'elasticsearch'
-//       }
-//     }
-//   }
-// }).then( () => {
-//   for (const tweet of response.hits.hits) {
-//     console.log('tweet:', tweet);
-//   }
 // })
+// .on("end", function() {
+//       console.log("All Finished! 😄");
+// });
+
