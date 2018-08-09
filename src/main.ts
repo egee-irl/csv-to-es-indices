9dd59cd0413e0f27e5cd11dd687497a2c6ce2bc9
egee-irl <brian@egee.io>
2018-08-08 18:35:23 -0700

    paramed things

--- src/main.ts
+++ src/main.ts
@@ -2,39 +2,46 @@ import * as elasticsearch from "elasticsearch";
 import * as fs from "fs";
 import csv from "fast-csv";
 
-const stream = fs.createReadStream("1987.csv");
+const index_name = "helloworld";
+const type_name = "thing";
+const csv_path = "1987.csv";
+
 const client = new elasticsearch.Client({
   host: "https://search-eg-cluster-44ixoq4bigmmkxfr6akgx2qe3m.us-west-2.es.amazonaws.com/",
   log: "trace"
 });
 
-const exists = client.exists({
-  index: "helloworld",
-  type: "thing",
+client.exists({
+  index: index_name,
+  type: type_name,
   id: "1"
 }).then((r: any) => {
   if (r == false) {
     client.indices.create({
-      "index": "helloworld"
+      "index": index_name
+    }).catch((r) => {
+      console.log("Something went terribly wrong 😢" + r);
     });
   }
 });
 
-// const csvStream = csv({
-//   headers: true,
-//   ignoreEmpty: true
-// }).on("data", function(data: any) {
-//   client.index({
-//     index: "helloworld",
-//     type: "thing",
-//     body: {
-//       data
-//     }
-//   }).catch((r: any) => {
-//     console.log(r);
-//   });
-// })
-// .on("end", function() {
-//       console.log("All Finished! 😄");
-// });
+const stream = fs.createReadStream(csv_path);
+const csvStream = csv({
+  headers: true,
+  ignoreEmpty: true
+}).on("data", function(data: any) {
+  client.index({
+    index: index_name,
+    type: type_name,
+    body: {
+      data
+    }
+  }).catch((r: any) => {
+    console.log(r);
+  });
+})
+.on("end", function() {
+      console.log("All Finished! 😄");
+});
 
+stream.pipe(csvStream);
